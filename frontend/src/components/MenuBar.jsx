933ce7d3c0c175fd8fa54c2cef73d8007e3fa2b6
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-08-06 11:34:39 -0300

    filters button added on mobile menu

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,12 +1,22 @@
+import HamburgerIcon from './icons/HamburgerIcon';
+import PersonCircleIcon from './icons/PersonCircleIcon';
+import WorldIcon from './icons/WorldIcon';
 import style from './MenuBar.module.css';
 
 const MenuBar = () => {
   return (
-    <div className={style.menuBar}>
-      <p>Sé anfitrión</p>
-      <p>ID</p>
-      <p>user</p>
-    </div>
+    <nav className={style.menuBar}>
+      <a href='/host/homes' className={style.link}>
+        Sé anfitrión
+      </a>
+      <button className={style.worldButton}>
+        <WorldIcon width='1rem' />
+      </button>
+      <button className={style.userButton}>
+        <HamburgerIcon width='1rem' />
+        <PersonCircleIcon width='2rem' />
+      </button>
+    </nav>
   );
 };
 
--- frontend/src/components/MobileSearchButton.jsx
+++ frontend/src/components/MobileSearchButton.jsx
@@ -1,22 +1,30 @@
+import FiltersIcon from './icons/FiltersIcon';
 import SearchIcon from './icons/SearchIcon';
 import style from './MobileSearchButton.module.css';
 
 const MobileSearchButton = ({ ...props }) => {
   return (
-    <button className={style.mobileButton} {...props} data-option='where'>
-      <span className={style.searchIcon}>
-        <SearchIcon />
-      </span>
-      <span className={style.textContainer}>
-        <span className={style.title}>¿A dónde vas?</span>
-        <span className={style.detailContainer}>
-          <span>A cualquier lugar</span>
-          <span>semana (en cualquier fecha)</span>
-          <span>¿Cuántos?</span>
+    <div className={style.buttonsContainer}>
+      <button className={style.mobileButton} {...props} data-option='where'>
+        <span className={style.searchIcon}>
+          <SearchIcon />
         </span>
-      </span>
-      <span></span>
-    </button>
+        <span className={style.textContainer}>
+          <span className={style.title}>¿A dónde vas?</span>
+          <span className={style.detailContainer}>
+            <span className={style.detailText}>A cualquier lugar</span>
+            <span className={style.detailText}>
+              semana (en cualquier fecha)
+            </span>
+            <span className={style.detailText}>¿Cuántos?</span>
+          </span>
+        </span>
+        <span></span>
+      </button>
+      <button className={style.filtersButton} {...props} data-option='filters'>
+        {<FiltersIcon width='1.5rem' />}
+      </button>
+    </div>
   );
 };
 
